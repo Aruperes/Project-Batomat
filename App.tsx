de7f2fb3331db83b2722ecfb78d47f2b7136103f
Aruperes <revandoaruperes6@gmail.com>
2024-10-22 20:13:52 +0800

    signup

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import SplashScreen from './src/pages/SplashScreen';
+import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
 
 const App = () => {
-  return <SplashScreen />;
+  return <SignUp />;
 };
 
 export default App;
--- src/assets/icon/index.tsx
+++ src/assets/icon/index.tsx
@@ -1,2 +1,5 @@
 import Logo from './Frame.svg';
 export {Logo};
+
+import Logo1 from './BackButton.svg';
+export {Logo1};
