ce8e203947bd4059c8e51775c377aa2e5baac677
Noselycha <noselychasoriton04@gmail.com>
2024-11-15 12:07:39 +0700

    add swiper,search

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,14 @@
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 import React from 'react';
 import {Logo1} from '../../assets/icon';
+import Swiper from 'react-native-swiper';
 
 const SignIn = ({navigation}) => {
   return (
@@ -16,11 +24,54 @@ const SignIn = ({navigation}) => {
           source={require('../../assets/images/profile.png')}
         />
       </View>
-      <View style={styles.contentWrapper}>
-        <Text style={styles.head}>
-          Sign <Text style={styles.headBold}>In</Text>
-        </Text>
+      <View style={styles.line} />
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search"
+          placeholderTextColor="#a0a0a0"
+        />
+        <Image
+          style={styles.searchIcon}
+          source={require('../../assets/icon/search.svg')} // Ganti dengan path ikon pencarian Anda
+        />
       </View>
+      <ScrollView>
+        <View style={styles.box1}>
+          <Swiper style={styles.wrapper}>
+            <View testID="Hello" style={styles.slide1}>
+              <Text style={styles.text}>Tomat pasti segar!</Text>
+              <Text style={styles.text2}>
+                inilah obat yang membuat buah tomat lebih segar dan sehat
+              </Text>
+              <Image
+                style={styles.image}
+                source={require('../../assets/images/solanum.png')}
+              />
+            </View>
+            <View testID="Beautiful" style={styles.slide2}>
+              <Text style={styles.text}>Perawatan yang baik</Text>
+              <Text style={styles.text2}>
+                Dengan perawatan yang baik membuat pertumbuhan lebih subur
+              </Text>
+              <Image
+                style={styles.image}
+                source={require('../../assets/images/nutrition.png')}
+              />
+            </View>
+            <View testID="Simple" style={styles.slide3}>
+              <Text style={styles.text}>Peran penting obat</Text>
+              <Text style={styles.text2}>
+                obat mencegah penyakit yang ada pada tumbuhan
+              </Text>
+              <Image
+                style={styles.image}
+                source={require('../../assets/images/obatomat.png')}
+              />
+            </View>
+          </Swiper>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -37,29 +88,84 @@ const styles = StyleSheet.create({
   logo: {
     width: 115, // Atur ukuran logo sesuai kebutuhan
     height: 35,
-    marginHorizontal: 100, // Memberi jarak antara kedua logo
+    marginHorizontal: 110, // Memberi jarak antara kedua logo
     marginTop: 21,
   },
   logo1: {
     width: 40, // Atur ukuran logo sesuai kebutuhan
     height: 40,
-    marginHorizontal: 85, // Memberi jarak antara kedua logo
+    marginHorizontal: 110, // Memberi jarak antara kedua logo
     marginTop: 21,
   },
-  contentWrapper: {
-    width: '90%',
-    padding: 24,
-    borderRadius: 20,
+  line: {
+    borderBottomColor: '#C5C5C5',
+    borderBottomWidth: 1,
+    width: 325,
+    marginVertical: 10,
   },
-  head: {
-    fontFamily: 'Poppins-Regular',
-    fontSize: 32,
-    textAlign: 'center',
-    color: 'black',
-    marginBottom: 24,
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#F2F2F2', // Warna background search bar
+    borderRadius: 8,
+    padding: 10,
+    shadowColor: '#000', // Shadow untuk efek
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2, // Untuk shadow di Android
+    width: 370,
+    height: 50,
   },
-  headBold: {
-    fontFamily: 'Poppins-Bold',
+  searchInput: {
+    flex: 1,
+    marginTop: -10,
+    fontSize: 16,
+    color: '#333',
+  },
+  searchIcon: {
+    width: 20,
+    height: 20,
+    marginTop: 10,
+    tintColor: '#292D32',
+  },
+  box1: {
+    height: 285,
+  },
+  wrapper: {
+    alignItems: 'center',
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F9F7E4',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F9F7E4',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F9F7E4',
+  },
+  text: {
+    color: '#292D32',
+    fontSize: 24,
+    marginTop: -50,
+    marginLeft: 27,
+    fontFamily: 'Poppins-SemiBold',
+  },
+  text2: {
+    color: '#76756C',
+    fontSize: 12,
+    width: 200,
+    marginLeft: 30,
+  },
+  image: {
+    marginLeft: 80,
+    marginTop: 10,
   },
 });
 
