73d3d6416043ead4a48a07dc9328df4bba9ae5d3
Aruperes <revandoaruperes6@gmail.com>
2024-11-17 20:09:07 +0800

    reset2

--- src/components/atoms/index.tsx
+++
@@ -1,9 +0,0 @@
-import Button from './Button';
-import Btn from './ButtonIntro';
-import Gap from './Gap';
-import GapRow from './GapRow';
-import ButtonOp from './ButtonOp';
-import DateNote from './DateNote';
-import AddNote from './AddNote';
-
-export {Button, Gap, GapRow, DateNote, AddNote, Btn, ButtonOp};
--- src/components/molecules/index.tsx
+++
@@ -1,7 +0,0 @@
-import Header from './Header';
-import TextInput from './TextInput';
-import MenuButton from './MenuButton';
-import Floating from './Floating';
-import LookNote from './LookNote';
-
-export {Header, TextInput, MenuButton, LookNote, Floating};
