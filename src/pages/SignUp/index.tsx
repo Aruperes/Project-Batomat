a0133aa0b8cc7a722efd953e1920e3a13fadb600
Aruperes <revandoaruperes6@gmail.com>
2024-11-18 14:24:56 +0800

    add auth firebase

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,39 @@
 import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import React, {useState} from 'react';
-import {TextInput} from '../../components/molecules';
+import {TextInput, Loading} from '../../components/molecules';
 import {Button, Gap} from '../../components/atoms';
+import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
+import {showMessage} from 'react-native-flash-message';
 
 const SignUp = ({navigation}) => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const createUser = () => {
+    const auth = getAuth();
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+
+        showMessage({
+          message: 'Account created successfully!',
+          type: 'success',
+        });
+        navigation.navigate('SignIn');
+      })
+      .catch(error => {
+        showMessage({
+          message: error.message,
+          type: 'danger',
+        });
+        // ..
+      });
+  };
+
   return (
     <View style={styles.overlay}>
       <Image source={require('../../assets/images/LogoIm.png')} />
@@ -11,20 +41,29 @@ const SignUp = ({navigation}) => {
         <Text style={styles.head}>
           Sign <Text style={styles.headBold}>Up</Text>
         </Text>
-        <TextInput label="Full Name" placeholder="Type your full name" />
+        <TextInput
+          label="Full Name"
+          placeholder="Type your full name"
+          onChangeText={value => setFullName(value)}
+        />
         <Gap height={16} />
         <TextInput
           label="Email Address"
           placeholder="Type your email address"
+          onChangeText={e => setEmail(e)}
         />
         <Gap height={16} />
-        <TextInput label="Password" placeholder="Type your password" />
+        <TextInput
+          label="Password"
+          placeholder="Type your password"
+          onChangeText={e => setPassword(e)}
+        />
         <Gap height={24} />
         <Button
           text="Create"
           color="#2F2A36"
           textColor="#F0DFBD"
-          onPress={() => navigation.navigate('SignIn')}
+          onPress={createUser}
         />
       </View>
     </View>
